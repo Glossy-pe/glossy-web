72071de6bf10780586397e240009813cb0631925
Ricardo Anderson Yalico Roncal <u201920140@upc.edu.pe>
2026-02-08 20:48:25 -0500

    Agregando componentes de Administracion V1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,9 @@ import { ProductDetail } from './features/product/components/product-detail/prod
 import { CartList } from './features/cart/components/cart-list/cart-list';
 import { Home } from './features/home/components/home/home';
 import { ValentineDay } from './features/valentin-day/components/valentine-day/valentine-day';
-import { ImagesList } from './admin-features/images/components/images-list/images-list';
+import { AdminImagesList } from './admin-features/images/components/images-list/admin-images-list';
+import { AdminProductList } from './admin-features/product/components/product-list/admin-product-list';
+import { AdminCategoryList } from './admin-features/category/components/category-list/admin-category-list';
 
 export const routes: Routes = [
     { path: 'home', component: Home },
@@ -13,7 +15,9 @@ export const routes: Routes = [
     { path: 'products', component: ProductList},
     { path: 'products/:id', component: ProductDetail },
     { path: 'san-valentin', component: ValentineDay },
-    { path: 'admin/images', component: ImagesList },
+    { path: 'admin/images', component: AdminImagesList },
+    { path: 'admin/products', component: AdminProductList },
+    { path: 'admin/categories', component: AdminCategoryList },
     { path: 'cart', component: CartList},
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: '**', redirectTo: '/products' }
