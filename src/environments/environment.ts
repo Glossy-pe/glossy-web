3c3f78543c241ecac09defff841c9c000169f523
Ricardo Anderson Yalico Roncal <u201920140@upc.edu.pe>
2026-02-11 18:46:13 -0500

    Agregando URL OAuth2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080',
-  apiImageServer: 'http://localhost:8000'
+  apiImageServer: 'http://localhost:8000',
+  apiOAuth2Server: 'http://localhost:9000',
 };
