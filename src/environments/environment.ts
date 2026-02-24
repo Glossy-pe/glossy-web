2f3cd59d6216edfa5d182adb244155743e464315
Ricardo Anderson Yalico Roncal <u201920140@upc.edu.pe>
2026-02-24 16:27:25 -0500

    Feature: Editar Variantes una vez agregadas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,6 @@ export const environment = {
   // apiUrl: 'http://localhost:8080',
   apiUrl: 'https://glossy-backend-server.mimarca.pe',
   apiImageServer: 'http://localhost:8000',
-  apiOAuth2Server: 'http://localhost:9000',
+  // apiOAuth2Server: 'http://localhost:9000',
+  apiOAuth2Server: 'https://glossy-oauth2-server.mimarca.pe',
 };
